d65c661fa5a79ce7b0867b109bf270c2c66013ad
Kevin Chen <me@kevchentw.com>
2017-07-23 21:01:47 +0800

    update

--- server/producer.py
+++ server/producer.py
@@ -1,26 +1,31 @@
 import asyncio
 from aioredis import create_connection, Channel, create_redis
 import websockets
+from ssl import SSLContext
 
 async def sub_server(websocket, path):
+    print('sub', websocket)
     sub = await create_redis(
-            ('localhost', 6379))
+            ('localhost', 6379), password="j6ck4xjp6")
     res = await sub.subscribe(path)
     channel = res[0]
     try:
         while True:
             message = await channel.get()
+            print('sub', message)
             await websocket.send(message.decode('utf-8'))
 
     except websockets.exceptions.ConnectionClosed:
-        await conn.unsubscribe(channel)
+        await sub.unsubscribe(channel)
         sub.close()
 
 async def pub_server(websocket, path):
+    print('pub', websocket)
     try:
         while True:
             message = await websocket.recv()
-            pub = await create_redis(('localhost', 6379))
+            print('pub', message)
+            pub = await create_redis(('localhost', 6379), password="j6ck4xjp6")
             res = await pub.publish(path, message)
             await asyncio.sleep(1)
 
@@ -30,8 +35,13 @@ async def pub_server(websocket, path):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-    ws_sub = websockets.serve(sub_server, '0.0.0.0', 8767)
-    ws_pub = websockets.serve(pub_server, '0.0.0.0', 8765)
+    ssl = SSLContext()
+    ssl.load_cert_chain(
+        '/etc/letsencrypt/live/letspotify.nctu.me/fullchain.pem',
+        '/etc/letsencrypt/live/letspotify.nctu.me/privkey.pem',
+    )
+    ws_sub = websockets.serve(sub_server, '0.0.0.0', 8767, ssl=ssl)
+    ws_pub = websockets.serve(pub_server, '0.0.0.0', 8765, ssl=ssl)
     loop.run_until_complete(ws_sub)
     loop.run_until_complete(ws_pub)
     loop.run_forever()
